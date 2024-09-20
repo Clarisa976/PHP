b4c90de0bf83c3f2f2c7df62d3897cea38487fe4
Clara <calvara976@g.educaand.es>
2024-09-20 10:13:18 +0200

    terminar la practica 1c hay que mantener lo seleccionado en el sexo y la ciudad

---
+++ Pract_1C/index.php
@@ -0,0 +1,37 @@
+<?php
+    if(isset($_POST["btnEnviar"])){
+        //comprobamos errores en el formulario
+        $error_nombre=$_POST["nombre"]=="";
+        $error_apellidos=$_POST["apellidos"]=="";
+        $error_pass=$_POST["pass"]=="";
+        $error_DNI=$_POST["DNI"]=="";
+        $error_sexo=!isset($_POST["sexo"]);
+        $error_comentarios=$_POST["comentarios"]=="";
+        $errores_form=$error_nombre
+        ||$error_apellidos || $error_pass
+        ||$error_DNI || $error_sexo || $error_comentarios;
+    }
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 1</title>
+    <style>
+        .error{color:red};
+    </style>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST["btnEnviar"]) && !$errores_form) {
+        require "vistas/vista_recogida.php";
+    } else {
+        require "vistas/vista_formulario.php";
+    }
+?>
+</body>
+
+</html>
