c106f8cb7f487c3243818b26db739d848efcc378
Clara <calvara976@g.educaand.es>
2024-10-18 10:14:06 +0200

    practicar con el examen

---
+++ Practica_Examen_2122/ejercicio1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+    <form action="ejercicio1.php" method="post">
+        <p>
+            <label for="texto">Introduce un texto</label>
+            <input type="text" name="texto" id="texto" value="<?php if (isset($_POST['texto'])) echo $_POST['texto'] ?>">
+
+        </p>
+        <p>
+            <button type="submit" name="bntEnviar">Enviar</button>
+        </p>
+    </form>
+    <?php
+    if (isset($_POST["btnEnviar"])) {
+        $texto = $_POST["texto"];
+        function mi_strlend($texto)
+        {
+            $contador = 0;
+            while (isset($texto[$contador])) {
+                $contador++;
+            }
+            return $contador;
+        }
+
+        echo "<p> Has introducido " . mi_strlend($texto) . " caracteres</p>";
+    }
+    ?>
+
+
+</body>
+
+</html>
