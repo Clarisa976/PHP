3db302b6213ace4235330f293ced38e007a630a5
Clara <calvara976@g.educaand.es>
2024-11-25 14:41:20 +0100

    no estoy segura

--- Tema2/Practica_Examen/Examen2/vistas/vista_tabla.php
+++ Tema2/Practica_Examen/Examen2/vistas/vista_tabla.php
@@ -1,29 +1,5 @@
 <?php
-if (isset($_SESSION["horario_profe"])) {
-    $profe = $_SESSION["horario_profe"];
-} else {
-    $profe = $_POST["horario_profe"];
-}
-
-//consulta para seleccionar las horas, el dia, los grupos del profe seleccionado
-try {
-    $consulta = "select usuario, dia, hora, grupo, nombre from grupos, horario_lectivo where id_grupo=grupo and usuario='".$profe."'";
-    $resultado = mysqli_query($conexion,$consulta);
-} catch (Exception $e) {
-    mysqli_close($conexion);
-    session_destroy();
-    die("Examen2,<p>No se ha podido conectar a la BD: " . $e->getMessage() . "</p>");
-}
-while($tupla = mysqli_fetch_assoc($resultado)){
-    if (isset($horario_profe[$tupla["hora"]][$tupla["dia"]])) {
-        //si hay más de un grupo 
-        $horario_profe[$tupla["hora"]][$tupla["dia"]] .= " / " . $tupla["nombre"];
-    } else {
-        $horario_profe[$tupla["hora"]][$tupla["dia"]] = $tupla["nombre"];
-    }
-}
 //tabla
-
 $horas[1] = "8:15-9:15";
 $horas[] = "9:15-10:15";
 $horas[] = "10:15-11:15";
@@ -39,19 +15,26 @@ foreach ($semana as $dia) {
     echo "<th>$dia</th>";
 }
 echo "</tr>";
-for ($hora = 1; $hora <= 7; $hora++) {
+for ($hora = 1; $hora <= count($horas); $hora++) {
     echo "<tr>";
     echo "<th>" . $horas[$hora] . "</th>";
     //si es la cuarta hora le ponemos recreo
     if ($hora == 4) {
         echo "<td colspan='5'>RECREO</td>";
     } else {
-        for ($dia = 1; $dia <= 5; $dia++) {
-            if (isset($horario_profe[$dia][$hora])) {
-                echo "<td>" . $horario_profe[$dia][$hora] . "</td>";
-            } else {
-                echo "<td></td>";
-            }
+        for ($dia = 1; $dia <= count($semana); $dia++) {
+            echo "<td>";
+            if (isset($horario_profe[$dia][$hora])) 
+                echo $horario_profe[$dia][$hora];
+           
+                echo "<form action='index.php' method='post'>";
+                echo "<input type='hidden' name='dia' value='".$dia."'>";
+                echo "<input type='hidden' name='hora' value='".$hora."'>";
+                echo "<input type='hidden' name='horario_profe' value='".$_POST["horario_profe"]."'>";
+                echo "<button class='enlace' type='submit' name='btnEditar'>Editar</button>";
+                echo "</form>";
+            
+            echo "</td>";
         }
     }
     echo "</tr>";
