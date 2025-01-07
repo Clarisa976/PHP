35255fe1f3fe4980c70fafbd585ebeee76c955e2
Clara <calvara976@g.educaand.es>
2025-01-07 13:40:13 +0100

    año nuevo, pantalla nueva

--- Tema5/Teoria_SW/API/index.php
+++ Tema5/Teoria_SW/API/index.php
@@ -4,11 +4,13 @@ require __DIR__ . '/Slim/autoload.php';
 
 $app= new \Slim\App;
 
-$app->get('/saludo/{codigo}',function($request){
+$app->get('/saludo',function($request){
 
+    
     //$datos["cod"]=$request->getParam('cod');
-    echo json_encode(array("mensaje"=> "Hola ".$request->getAttribute('codigo')) ,JSON_FORCE_OBJECT);
-
+    //echo json_encode(array("mensaje"=> "Hola ".$request->getAttribute('codigo')) ,JSON_FORCE_OBJECT);
+    $respuesta["mensaje"]="Hola, buenos dias";
+    echo json_encode($respuesta);
 });
 
 // Una vez creado servicios los pongo a disposición
