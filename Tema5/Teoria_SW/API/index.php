cf1cf11d47b2e1544c671ccdb8f5f84e20a550b0
Clara <calvara976@g.educaand.es>
2025-01-07 14:42:32 +0100

    Implementar métodos GET, POST, PUT y DELETE en la API de saludos y crear una aplicación web para consumir la API

--- Tema5/Teoria_SW/API/index.php
+++ Tema5/Teoria_SW/API/index.php
@@ -4,15 +4,39 @@ require __DIR__ . '/Slim/autoload.php';
 
 $app= new \Slim\App;
 
-$app->get('/saludo',function($request){
+$app->get('/saludo',function(){
 
-    
-    //$datos["cod"]=$request->getParam('cod');
-    //echo json_encode(array("mensaje"=> "Hola ".$request->getAttribute('codigo')) ,JSON_FORCE_OBJECT);
-    $respuesta["mensaje"]="Hola, buenos dias";
+    $respuesta["mensaje"]="Hola que tal?";
     echo json_encode($respuesta);
 });
 
-// Una vez creado servicios los pongo a disposición
+$app->get('/saludo/{nombre}',function($request){
+    $nombre=$request->getAttribute("nombre");
+    $respuesta["mensaje"]="Hola que tal, ".$nombre."?";
+    echo json_encode($respuesta);
+});
+
+$app->post('/saludo',function($request){
+    //por debajo
+    $nombre=$request->getParam("nombre");
+    $respuesta["mensaje"]="Hola que tal, ".$nombre."?";
+    echo json_encode($respuesta);
+});
+//añadir un método put y otro delete.
+//delete('/borrar_saludo/{id}) y que aparezca 'se ha borrado el saludo num X
+$app->delete('/borrar_saludo/{id}',function($request){
+    $id=$request->getAttribute("id");
+    $respuesta["mensaje"]="Se ha borrado el saludo con id ".$id;
+    echo json_encode($respuesta);
+});
+//put('/cambiar_saludo/{id}'....)y por debajo un nombre el mensaje sería: Se le ha actualizado el saludo con id X al nombre X
+$app->put('/cambiar_saludo/{id}',function($request){
+    $id=$request->getAttribute("id");
+    $nombre=$request->getParam("nombre");
+    $respuesta["mensaje"]="Se le ha actualizado el saludo con id ".$id." al nombre ".$nombre;
+    echo json_encode($respuesta);
+});
+
 $app->run();
+
 ?>
