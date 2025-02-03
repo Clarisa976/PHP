4bdcfbccca3871738f3f7b14c4b1bc2559c4539b
Clara <calvara976@g.educaand.es>
2025-02-03 13:58:45 +0100

    teoria

---
+++ Tema6/Teoria_Ayax/js/index.js
@@ -0,0 +1,132 @@
+const DIR_API1 = "http://localhost/Proyectos/PHP/Tema5/Teoria_SW/API";
+const DIR_API2 = "http://localhost/Proyectos/PHP/Tema5/Ejercicios_SW/Actividad1_2_MA/Actividad1/servicios_rest";
+
+$(function () {
+    obtener_productos();
+});
+
+function obtener_productos() { 
+    $.ajax({
+        url: DIR_API2 + "/productos",
+        dataType: "json",
+        type: "GET"
+    })
+        .done(function (data) {
+            if (data.error) {
+                $('#respuesta').html(data.error);
+            } else {
+                let html_tabla_productos = "<table>";
+                html_tabla_productos += "<tr><th>Código</th><th>Nombre</th><th>PVP</th></tr>";
+                $.each(data.productos, function (key, tupla) { 
+                    html_tabla_productos += "<tr>";
+                    html_tabla_productos += "<td>" + tupla["cod"] + "</td>";
+                    html_tabla_productos += "<td>" + tupla["nombre_corto"] + "</td>";
+                    html_tabla_productos += "<td>" + tupla["PVP"] + "</td>";
+                    html_tabla_productos += "</tr>";
+                });
+                html_tabla_productos += "</table>";
+                $('#respuesta').html(html_tabla_productos);
+            }
+        })
+        .fail(function (a, b) {
+            $("#respuesta").html(error_ajax_jquery(a, b));
+        });
+}
+
+function llamada_get1() {
+    $.ajax({
+        url: DIR_API1 + "/saludo",
+        dataType: "json",
+        type: "GET"
+    })
+        .done(function(data){
+            $('#respuesta').html(data.mensaje);
+        })
+        .fail(function (a, b) {
+            $("#respuesta").html(error_ajax_jquery(a, b));
+        });
+
+}
+
+function llamada_get2() {
+    let nombre_envio = 'María José';
+    $.ajax({
+        url: encodeURI(DIR_API1 + "/saludo/"+nombre_envio),
+        dataType: "json",
+        type: "GET"
+    })
+        .done(function (data) {
+            $('#respuesta').html(data.mensaje);
+        })
+        .fail(function (a, b) {
+            $("#respuesta").html(error_ajax_jquery(a, b));
+        });
+
+}
+
+function llamada_post1() {
+    let nombre_envio = 'María José';
+    $.ajax({
+        url: DIR_API1 + "/saludo",
+        dataType: "json",
+        type: "POST",
+        data: {"nombre":nombre_envio}
+    })
+        .done(function (data) {
+            $('#respuesta').html(data.mensaje);
+        })
+        .fail(function (a, b) {
+            $("#respuesta").html(error_ajax_jquery(a, b));
+        });
+
+}
+
+function llamada_delete1() {
+    let id = '9';
+    $.ajax({
+        url: DIR_API1 + "/borrar_saludo/"+id,
+        dataType: "json",
+        type: "DELETE",
+        //data: { "nombre": nombre_envio }
+    })
+        .done(function (data) {
+            $('#respuesta').html(data.mensaje);
+        })
+        .fail(function (a, b) {
+            $("#respuesta").html(error_ajax_jquery(a, b));
+        });
+
+}
+function llamada_put1() {
+    let id = '9';
+    let nombre_envio = 'María José';
+    $.ajax({
+        url: DIR_API1 + "/cambiar_saludo/" + id,
+        dataType: "json",
+        type: "PUT",
+        data: { "nombre": nombre_envio }
+    })
+        .done(function (data) {
+            $('#respuesta').html(data.mensaje);
+        })
+        .fail(function (a, b) {
+            $("#respuesta").html(error_ajax_jquery(a, b));
+        });
+
+}
+
+function error_ajax_jquery(jqXHR, textStatus) {
+    var respuesta;
+    if (jqXHR.status === 0) {
+        respuesta = 'Not connect: Verify Network.';
+    }
+    else if (jqXHR.status == 404) {
+        respuesta = 'Requested page not found [404]';
+    }
+    else if (jqXHR.status === 500) {
+        respuesta = 'Requested page not found [500]';
+    }
+    else if (textStatus === 'parseerror') {
+        respuesta = 'Requested JSON parse failed.';
+    }
+}
